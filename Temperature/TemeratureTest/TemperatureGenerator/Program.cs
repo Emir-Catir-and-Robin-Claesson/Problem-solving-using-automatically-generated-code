2155cfde2146c4ad7d8132e47eb95ad398856eb7
Robin Claesson <57729281+RobinClaesson@users.noreply.github.com>
2023-03-22 14:49:01 +0100

    Temperature Problem Generator

---
+++ Temperature/TemeratureTest/TemperatureGenerator/Program.cs
@@ -0,0 +1,16 @@
+﻿var random = new Random();
+
+var numOfWeeks = random.Next(4, 11);
+var numOfMeassurements = random.Next(3, 7);
+
+Console.WriteLine($"{numOfWeeks} {numOfMeassurements}");
+
+for(int i = 0; i < numOfWeeks; i++)
+{
+    for(int j = 0; j < numOfMeassurements; j++)
+    {
+        Console.Write($"{random.Next(-10,31)} ");
+    }
+
+    Console.WriteLine();
+}
