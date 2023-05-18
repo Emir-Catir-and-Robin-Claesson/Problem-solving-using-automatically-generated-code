f9aca51ffcbcef46fc8d736aa58b146a2fc5ae04
Robin Claesson <robcla@kth.se>
2023-05-18 11:42:45 +0200

    Improved Temperature test

--- ProgramTests/TemperatureTest/TemperatureTest.cs
+++ ProgramTests/TemperatureTest/TemperatureTest.cs
@@ -57,17 +57,19 @@ namespace TemperatureProblemTest
             var numOfMeasurements = int.Parse(firstInputRowSplit[1]);
 
 
-            var globalMin = double.MaxValue;
-            var globalMax = double.MinValue;
-            var globalTotal = 0.0;
+            var globalMin = decimal.MaxValue;
+            var globalMax = decimal.MinValue;
+            var globalTotal = 0.0m;
+
+            var roundingErrors = new StringBuilder();
 
             for (int i = 0; i < numOfWeeks; i++)
             {
-                var temps = inputRows[1 + i].TrimEnd().Split(' ').Select(m => double.Parse(m, _culture)).ToList();
+                var temps = inputRows[1 + i].TrimEnd().Split(' ').Select(m => decimal.Parse(m, _culture)).ToList();
 
-                var localMin = double.MaxValue;
-                var localMax = double.MinValue;
-                var localTotal = 0.0;
+                var localMin = decimal.MaxValue;
+                var localMax = decimal.MinValue;
+                var localTotal = 0.0m;
 
                 for (int j = 0; j < numOfMeasurements; j++)
                 {
@@ -85,37 +87,95 @@ namespace TemperatureProblemTest
                     globalMin = localMin;
                 globalTotal += localTotal;
 
-                var localAvg = Math.Round(localTotal / numOfMeasurements, 1);
+                var localAvg = Decimal.Round(localTotal / numOfMeasurements, 1, MidpointRounding.AwayFromZero);
 
                 var outputData = outputRows[i].TrimEnd().Split();
-                var outputMin = double.Parse(outputData[0], _culture);
-                var outputMax = double.Parse(outputData[1], _culture);
-                var outputAvg = double.Parse(outputData[2], _culture);
+                var outputMin = decimal.Parse(outputData[0], _culture);
+                var outputMax = decimal.Parse(outputData[1], _culture);
+                var outputAvg = decimal.Parse(outputData[2], _culture);
 
                 if (outputMin != localMin)
-                    return (false, $"Wrong local min on week '{i + 1}': Gave '{outputMin}' but should have been '{localMin}'");
-                if (outputMax != localMax)
-                    return (false, $"Wrong local max on week '{i + 1}': Gave '{outputMax}' but should have been '{localMax}'");
-                if (outputAvg != localAvg)
-                    return (false, $"Wrong local avg on week '{i + 1}': Gave '{outputAvg}' but should have been '{localAvg}'");
+                {
+                    if (Math.Abs(outputMin - localMin) < 0.2m)
+                    {
+                        roundingErrors.AppendLine($"Wrong local min on week '{i + 1}': Gave '{outputMin}' but should have been '{localMin}' | ");
+                    }
+                    else
+                    {
+                        return (false, $"Wrong local min on week '{i + 1}': Gave '{outputMin}' but should have been '{localMin}'");
+                    }
+                }
 
+                if (outputMax != localMax)
+                {
+                    if (Math.Abs(outputMax - localMax) < 0.2m)
+                    {
+                        roundingErrors.AppendLine($"Wrong local max on week '{i + 1}': Gave '{outputMax}' but should have been '{localMax}' | ");
+                    }
+                    else
+                    {
+                        return (false, $"Wrong local max on week '{i + 1}': Gave '{outputMax}' but should have been '{localMax}'");
+                    }
+
+                }
+
+                if (outputAvg != localAvg)
+                {
+                    if (Math.Abs(outputMax - localMax) < 0.2m)
+                    {
+                        roundingErrors.AppendLine($"Wrong local avg on week '{i + 1}': Gave '{outputAvg}' but should have been '{localAvg}' | ");
+                    }
+                    else
+                    {
+                        return (false, $"Wrong local avg on week '{i + 1}': Gave '{outputAvg}' but should have been '{localAvg}'");
+
+                    }
+                }
             }
 
-            var globalAvg = Math.Round(globalTotal / (numOfMeasurements * numOfWeeks), 1);
+            var globalAvg = Decimal.Round(globalTotal / (numOfMeasurements * numOfWeeks), 1, MidpointRounding.AwayFromZero);
+
 
             var outputGlobalData = outputRows.Last().TrimEnd().Split();
-            var outputGlobalMin = double.Parse(outputGlobalData[0], _culture);
-            var outputGlobalMax = double.Parse(outputGlobalData[1], _culture);
-            var outputGlobalAvg = double.Parse(outputGlobalData[2], _culture);
+            var outputGlobalMin = decimal.Parse(outputGlobalData[0], _culture);
+            var outputGlobalMax = decimal.Parse(outputGlobalData[1], _culture);
+            var outputGlobalAvg = decimal.Parse(outputGlobalData[2], _culture);
 
             if (outputGlobalMin != globalMin)
-                return (false, $"Wrong global min: Gave '{outputGlobalMin}' but should have been '{globalMin}'");
+            {
+                if (Math.Abs(outputGlobalMin - globalMin) < 0.2m)
+                {
+                    roundingErrors.AppendLine($"Wrong global min: Gave '{outputGlobalMin}' but should have been '{globalMin}' | ");
+                }
+                else
+                {
+                    return (false, $"Wrong global min: Gave '{outputGlobalMin}' but should have been '{globalMin}'");
+                }
+            }
             if (outputGlobalMax != globalMax)
-                return (false, $"Wrong global max: Gave '{outputGlobalMax}' but should have been '{globalMax}'");
+            {
+                if (Math.Abs(outputGlobalMax - globalMax) < 0.2m)
+                {
+                    roundingErrors.AppendLine($"Wrong global max: Gave '{outputGlobalMax}' but should have been '{globalMax}' | ");
+                }
+                else
+                {
+                    return (false, $"Wrong global max: Gave '{outputGlobalMax}' but should have been '{globalMax}'");
+                }
+            }
             if (outputGlobalAvg != globalAvg)
-                return (false, $"Wrong global average: Gave '{outputGlobalAvg}' but should have been '{globalAvg}'");
+            {
+                if (Math.Abs(outputGlobalAvg - globalAvg) < 0.2m)
+                {
+                    roundingErrors.AppendLine($"Wrong global average: Gave '{outputGlobalAvg}' but should have been '{globalAvg} | '");
+                }
+                else
+                {
+                    return (false, $"Wrong global average: Gave '{outputGlobalAvg}' but should have been '{globalAvg}'");
+                }
+            }
 
-            return (true, string.Empty);
+            return (true, roundingErrors.ToString());
         }
     }
 }
